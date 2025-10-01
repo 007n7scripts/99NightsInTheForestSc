65c43664bc9715add897d4f04179f5fd83d433ea
007n7_SCRIPTS <007n7scripts@gmail.com>
2025-10-01 18:17:22 +0700

    Create ToastyXDScripts.lua

---
+++ ToastyXDScripts.lua
@@ -0,0 +1,2 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+loadstring(game:HttpGet("https://gitlab.com/007n7scripts/99nightsintheforest/-/raw/main/ToastyXDScript.lua"), true))()
